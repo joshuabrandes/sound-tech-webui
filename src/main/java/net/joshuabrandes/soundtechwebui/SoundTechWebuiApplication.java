e270fb863f41879f20062f6ed2f480d91ea361f5
Joshua Brandes <47532061+joshuabrandes@users.noreply.github.com>
2025-05-06 22:30:57 +0200

    first commit

---
+++ src/main/java/net/joshuabrandes/soundtechwebui/SoundTechWebuiApplication.java
@@ -0,0 +1,13 @@
+package net.joshuabrandes.soundtechwebui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundTechWebuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoundTechWebuiApplication.class, args);
+    }
+
+}
